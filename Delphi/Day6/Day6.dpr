d0ad3f5cd7e8f753eafb82a07f1a9be2b5ca16a0
Jos Visser <goleztrol@hotmail.com>
2021-12-06 21:05:41 +0100

    [Day 6] Various optimizations
    - Scratching the ToString at the end, and passing in the input as const string: 850 ns
    - Const array for Day7 and Day9, instead of calculating them in the iteration: 700 ns

--- Delphi/Day6/Day6.dpr
+++ Delphi/Day6/Day6.dpr
@@ -14,83 +14,70 @@ uses
   Math,
   System.Diagnostics,
   System.Generics.Collections;
+const
+  Days7: array[0..9] of Integer = (7, 8, 9, 0, 1, 2, 3, 4, 5, 6);
+  Days9: array[0..9] of Integer = (9, 0, 1, 2, 3, 4, 5, 6, 7, 8);
 
-
-function Simulate(Raw: TStringArray; Days: Integer): String;
+function Simulate(const Input: String; Days: Integer): Int64;
 var
   FishStates: array[0..9] of Int64;
   Sum: Int64;
 begin
-  Sum := 0;
   // Initialize an array of 'timers'/states, with how many fish are in that state
   ZeroMemory(@FishStates[0], (Length(FishStates) - 1) * SizeOf(FishStates[0]));
 
-  var Len := Length(Raw[0]);
+  var Len := Length(Input);
   var P := 1;
-  while P <= Len do
-  begin
-    Inc(FishStates[Ord(Raw[0][P]) - Ord('0')]);
+  repeat
+    Inc(FishStates[Ord(Input[P]) - Ord('0')]);
     Inc(P, 2);
-  end;
+  until P > Len;
 
   // Iterate for the number of days.
   var Day0 := 0;
-  var Day7 := 7;
-  var Day9 := 9;
   for var Day := 1 to Days do
   begin
-    FishStates[(Day9)] := FishStates[Day0]; // Spawn new fishes in a 'spawn buffer'
-    Inc(FishStates[(Day7)], FishStates[Day0]); // Reset timer for fished in 0, by adding them to 7
+    FishStates[(Days9[Day0])] := FishStates[Day0]; // Spawn new fishes in a 'spawn buffer'
+    Inc(FishStates[(Days7[Day0])], FishStates[Day0]); // Reset timer for fishes in 0, by adding them to 7
 
     // Instead of shifting the array of states (Day 1 becomes Day 0), increment
     // a day pointer instead. Keep separate pointers for days needed, and keep them
     // in 0 to 9 using an if, which beats mod 10 by _a lot_
-    Inc(Day0); Inc(Day7); Inc(Day9);
+    Inc(Day0);
     if Day0 = 10 then Day0 := 0;
-    if Day7 = 10 then Day7 := 0;
-    if Day9 = 10 then Day9 := 0;
   end;
 
-  // Sum the fish per state, excluding the ones in [9], which is the spawn buffer.
+  // Sum the fish per state, excluding the ones in the spawn buffer
+  Sum := - FishStates[Days9[Day0]];
   for var i := 0 to 9 do
-    if i <> Day9 then
-      Sum := Sum + FishStates[i];
-  Result := Sum.ToString;
-end;
-
-function Day6_1(Raw: TStringArray): String;
-begin
-  Result := Simulate(Raw, 80);
-end;
-
-function Day6_2(Raw: TStringArray): String;
-begin
-  Result := Simulate(Raw, 256);
+    Sum := Sum + FishStates[i];
+  Result := Sum;
 end;
 
 var
-  Result: String;
+  Input: String;
+  Result: Int64;
 begin
   WriteLn('Tests');
   Sleep(100);
-  var Input := LoadStrings('Day6.test.txt');
-  Result := Day6_1(Input);
-  Validate(Result, '5934');
-  Result := Day6_2(Input);
-  Validate(Result, '26984457539');
+  Input := LoadStrings('Day6.test.txt')[0];
+  Result := Simulate(Input, 80);
+  Validate(Result.ToString, '5934');
+  Result := Simulate(Input, 256);
+  Validate(Result.ToString, '26984457539');
 
   WriteLn(#10'Final');
-  Input := LoadStrings('Day6.input.txt');
-  Result := Day6_1(Input);
-  Validate(Result, '380758');
+  Input := LoadStrings('Day6.input.txt')[0];
+  Result := Simulate(Input, 80);
+  Validate(Result.ToString, '380758');
 
   var s := TStopwatch.StartNew;
   const Iterations = 1000000;
   for var i := 1 to Iterations do
-    Result := Day6_2(Input);
+    Result := Simulate(Input, 256);
   WriteLn(((s.ElapsedTicks * 1000000000) div Iterations) div s.Frequency, ' ns per simulation');
   WriteLn(Iterations, ' iterations in ', s.ElapsedMilliseconds, ' ms');
-  Validate(Result, '1710623015163');
+  Validate(Result.ToString, '1710623015163');
 
   WriteLn(#10'Hit it');
   ReadLn;
