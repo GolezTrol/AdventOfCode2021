2f8c5c3b5e1978b6cfe8334c999508645349007f
Jos Visser <goleztrol@hotmail.com>
2021-12-07 09:00:34 +0100

    [Day 6] Clean up and build to 64 bit
    Record as memory mapper is slower than variable assignment after all.
    Also, recompiling to 64 bits saves quite a lot. around 300 ns now! Saving of over 25%!

--- Delphi/Day6/Day6.dpr
+++ Delphi/Day6/Day6.dpr
@@ -18,72 +18,57 @@ const
   ExampleFile: String = '3,4,3,1,2';
   ActualFile: String = '3,3,2,1,4,1,1,2,3,1,1,2,1,2,1,1,1,1,1,1,4,1,1,5,2,1,1,2,1,1,1,3,5,1,5,5,1,1,1,1,3,1,1,3,2,1,1,1,1,1,1,4,1,1,1,1,1,1,1,4,1,3,3,1,1,3,1,3,1,2,1,3,1,1,4,1,2,4,4,5,1'+',1,1,1,1,1,4,1,5,1,1,5,1,1,3,3,1,3,2,5,2,4,1,4,1,2,4,5,1,1,5,1,1,1,4,1,1,5,2,1,1,5,1,1,1,5,1,1,1,1,1,3,1,5,3,2,1,1,2,2,1,2,1,1,5,1,1,4,5,1,4,3,1,1,1,1,1,1,5,1,1,1,5,'+'2,1,1,1,5,1,1,1,4,4,2,1,1,1,1,1,1,1,3,1,1,4,4,1,4,1,1,5,3,1,1,1,5,2,2,4,2,1,1,3,1,5,5,1,1,1,4,1,5,1,1,1,4'+',3,3,3,1,3,1,5,1,4,2,1,1,5,1,1,1,5,5,1,1,2,1,1,1,3,1,1,1,2,3,1,2,2,3,1,3,1,1,4,1,1,2,1,1,1,1,3,5,1,1,2,1,1,1,4,1,1,1,1,1,2,4,1,1,5,3,1,1,1,2,2,2,1,5,1,3,5,3,1,1,4,1,1,4';
 
-type
-  TFishStates = record
-    case Integer of
-    0: (F0, F1, F2, F3, F4, F5, F6, F7, F8: BigInt);
-    1: (FishStates: array[0..8] of BigInt);
-  end;
-
 function Simulate(const Input: String; Days: BestInt): BigInt;
 var
-  //FishStates: array[0..8] of BigInt;
-  f: TFishStates;
+  FishStates: array[0..8] of BigInt;
+  F0, F1, F2, F3, F4, F5, F6, F7, F8: BigInt;
 begin
-  f := Default(TFishStates);
-  with f do
-  begin
-  // Initialize an array of 'timers'/states, with how many fish are in that state
-
-  //ZeroMemory(@FishStates[0], (Length(FishStates)) * SizeOf(FishStates[0]));
+  ZeroMemory(@FishStates[0], (Length(FishStates)) * SizeOf(FishStates[0]));
 
   var P := Length(Input);
   repeat
-    Inc(FishStates[Ord(Input[P]) - Ord('0')]);
+    Inc(FishStates[Ord(Input[P]) - 48]);
     Dec(P, 2)
   until P < 0;
 
-    for var i := 1 to Days div 9 do
-    begin
-      Inc(F7, F0);
-      Inc(F8, F1);
-      Inc(F0, F2);
-      Inc(F1, F3);
-      Inc(F2, F4);
-      Inc(F3, F5);
-      Inc(F4, F6);
-      Inc(F5, F7);
-      Inc(F6, F8);
-    end;
+  F0 := FishStates[0];
+  F1 := FishStates[1];
+  F2 := FishStates[2];
+  F3 := FishStates[3];
+  F4 := FishStates[4];
+  F5 := FishStates[5];
+  F6 := FishStates[6];
+  F7 := FishStates[7];
+  F8 := FishStates[8];
 
+  for var i := 1 to Days div 9 do
+  begin
     Inc(F7, F0);
     Inc(F8, F1);
     Inc(F0, F2);
     Inc(F1, F3);
-    if Days = 80 then
-    begin
-      Inc(F2, F4);
-      Inc(F3, F5);
-      Inc(F4, F6);
-      Inc(F5, F7);
-      //Inc(F6, F8);
-    end;
-
-  {
-    var Day0 := 0;
-    for var Day := 1 to Days do
-    begin
-      Inc(FishStates[(Days7[Day0])], FishStates[Day0]);
-      Inc(Day0);
-      if Day0 = 9 then Day0 := 0;
-    end;}
-
-    // Sum the fish per state, excluding the ones in the spawn buffer
-    Result := 0;
-    for var i := 0 to 8 do
-      Inc(Result, FishStates[i]);
-    //Result := F0+F1+F2+F3+F4+F5+F6+F7+F8;
+    Inc(F2, F4);
+    Inc(F3, F5);
+    Inc(F4, F6);
+    Inc(F5, F7);
+    Inc(F6, F8);
   end;
+
+  Inc(F7, F0);
+  Inc(F8, F1);
+  Inc(F0, F2);
+  Inc(F1, F3);
+  if Days = 80 then
+  begin
+    Inc(F2, F4);
+    Inc(F3, F5);
+    Inc(F4, F6);
+    Inc(F5, F7);
+    //Inc(F6, F8);
+  end;
+
+  // Sum the fish per state, excluding the ones in the spawn buffer
+  Result := F0+F1+F2+F3+F4+F5+F6+F7+F8;
 end;
 
 var
